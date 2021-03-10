b7862091e1a99b103544910d623c6c4543c6932b
KaiserPhemi <kaiser.phemi@protonmail.ch>
2021-03-10 21:30:56 +0100

    chore()
    - add test files

--- src/task_2/task_2.test.js
+++ src/task_2/task_2.test.js
@@ -5,16 +5,30 @@ const checkPrevItem = require("./task_2");
 // utils
 const addItem = require("../utils/addItem");
 
+
 // assertions
-assert.deepStrictEqual(
-  checkPrevItem(["femi", "made", "it", "akinwa"], "made"),
-  "femi"
-);
-assert.deepStrictEqual(
-  checkPrevItem(["femi", "made", "it", "akinwa"], "femi"),
-  null
-);
-assert.deepStrictEqual(
-  checkPrevItem(["femi", "made", "it", "akinwa"], "it"),
-  "made"
-);
+
+describe('checkPrevItem module', ()=> {
+
+  it('should return previous string for an input in a list', ()=>{
+
+    assert.deepStrictEqual(
+      checkPrevItem(["femi", "made", "it", "akinwa"], "made"),
+      "femi"
+    );
+     assert.deepStrictEqual(
+       checkPrevItem(["femi", "made", "it", "akinwa"], "it"),
+       "made"
+     );
+  })
+  it('should return null if input string is the first element in the list', ()=> {
+    assert.deepStrictEqual(
+      checkPrevItem(["femi", "made", "it", "akinwa"], "femi"),
+      null
+    );
+  })
+ 
+})
+
+
+
